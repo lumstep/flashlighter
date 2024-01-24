972e4567cf7e5c7f3f3afce3893b43fa0c80d0fd
lumstep <hoodlum.stepka@gmail.com>
2024-01-24 14:47:21 +0100

    updated dependencies

--- flashlighter/build.gradle.kts
+++ flashlighter/build.gradle.kts
@@ -57,5 +57,5 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
 }
